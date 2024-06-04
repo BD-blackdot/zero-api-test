bd08c93ea6a9ee2f91758fcc420f538dfc4a94d2
BD-blackdot <76467443+BD-blackdot@users.noreply.github.com>
2024-06-04 11:58:04 +0900

    마진값 재수정

--- chat-button.js
+++ chat-button.js
@@ -22,7 +22,7 @@ function addChatButtonStyles() {
             border-radius: 50%;
             position: fixed;
             bottom: 20px;
-            right: 20px;
+            right: 80px;
             cursor: pointer;
             z-index: 1000;
             transition: all 0.5s ease-in-out;
@@ -46,7 +46,6 @@ function addChatButtonStyles() {
             transform: translateX(-50%);
             overflow: hidden;
             opacity: 0;
-            transition: all 0.5s ease-in-out;
         }
 
         #chat-input-container img {
